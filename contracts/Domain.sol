70d348f11845306f6e72103f5c29eb931ed4d80c
Jheave <jimenezjheave123@gmail.com>
2022-02-20 22:13:13 +0800

    change str lenght

--- contracts/Domain.sol
+++ contracts/Domain.sol
@@ -10,7 +10,6 @@ import {Base64} from "./libraries/Base64.sol";
 
 import "hardhat/console.sol";
 
-// inherit the contract imported.
 contract Domains is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
@@ -19,7 +18,7 @@ contract Domains is ERC721URIStorage {
 
   string public tld;
 	
-	// be storing our NFT images on chain as SVGs
+	// storing NFT images onchain as SVGs
   string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#a)" d="M0 0h270v270H0z"/><defs><filter id="b" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949a4.382 4.382 0 0 0-4.394 0l-10.081 6.032-6.85 3.934-10.081 6.032a4.382 4.382 0 0 1-4.394 0l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616 4.54 4.54 0 0 1-.608-2.187v-9.31a4.27 4.27 0 0 1 .572-2.208 4.25 4.25 0 0 1 1.625-1.595l7.884-4.59a4.382 4.382 0 0 1 4.394 0l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616 4.54 4.54 0 0 1 .608 2.187v6.032l6.85-4.065v-6.032a4.27 4.27 0 0 0-.572-2.208 4.25 4.25 0 0 0-1.625-1.595L41.456 24.59a4.382 4.382 0 0 0-4.394 0l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595 4.273 4.273 0 0 0-.572 2.208v17.441a4.27 4.27 0 0 0 .572 2.208 4.25 4.25 0 0 0 1.625 1.595l14.864 8.655a4.382 4.382 0 0 0 4.394 0l10.081-5.901 6.85-4.065 10.081-5.901a4.382 4.382 0 0 1 4.394 0l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616 4.54 4.54 0 0 1 .608 2.187v9.311a4.27 4.27 0 0 1-.572 2.208 4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721a4.382 4.382 0 0 1-4.394 0l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616 4.53 4.53 0 0 1-.608-2.187v-6.032l-6.85 4.065v6.032a4.27 4.27 0 0 0 .572 2.208 4.25 4.25 0 0 0 1.625 1.595l14.864 8.655a4.382 4.382 0 0 0 4.394 0l14.864-8.655a4.545 4.545 0 0 0 2.198-3.803V55.538a4.27 4.27 0 0 0-.572-2.208 4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="a" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop offset="0%" stop-color="#0cd7e4"><animate attributeName="stop-color" values="#0cd7e4; #0cd7e4; #7A5FFF" dur="5s" repeatCount="indefinite"/></stop><stop offset="1" stop-color="#cb5eee"><animate attributeName="stop-color" values="#01FF89; #7A5FFF; #01FF89" dur="4s" repeatCount="indefinite"/></stop></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#b)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
   string svgPartTwo = '</text></svg>';
 
@@ -59,6 +58,7 @@ contract Domains is ERC721URIStorage {
 		
 		// Combine the name passed into the function  with the TLD
     string memory _name = string(abi.encodePacked(name, ".", tld));
+
 		// Create the SVG (image) for the NFT with the name
     string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
     uint256 newRecordId = _tokenIds.current();
@@ -113,7 +113,6 @@ contract Domains is ERC721URIStorage {
   }
 
   function getAllNames() public view returns (string[] memory) {
-    console.log("Getting all names from contract");
     string[] memory allNames = new string[](_tokenIds.current());
     for (uint i = 0; i < _tokenIds.current(); i++) {
       allNames[i] = names[i];
@@ -124,7 +123,7 @@ contract Domains is ERC721URIStorage {
   }
   
   function valid(string calldata name) public pure returns(bool) {
-    return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 21;
+    return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 15;
   }
 
   modifier onlyOwner() {
