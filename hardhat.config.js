b3967e9f699ce8dd58eb814b95ae69147ce402e4
Jheave <jimenezjheave123@gmail.com>
2022-02-20 22:24:13 +0800

    add mumbai api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,10 +24,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
+    
     // mainnet: {
     //   chainId: 1,
     //   url: process.env.PROD_ALCHEMY_KEY,
     //   accounts: [process.env.PRIVATE_KEY],
     // },
   },
+  mumbai: {
+    apiKey: process.env.API_KEY,
+  } 
 };
